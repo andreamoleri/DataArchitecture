8339b3bea3682fad13349bcc0e89f2a859465113
Andrea Moleri <andreamoleri@gmail.com>
2024-05-10 13:52:08 +0200

    Added Javadoc

--- src/main/java/com/mongodb/quickstart/Connection.java
+++ src/main/java/com/mongodb/quickstart/Connection.java
@@ -1,3 +1,6 @@
+/**
+ * The Connection class provides functionality to connect to a MongoDB instance and list all available databases.
+ */
 package com.mongodb.quickstart;
 
 import com.mongodb.client.MongoClient;
@@ -9,10 +12,22 @@ import java.util.List;
 
 public class Connection {
 
+    /**
+     * The main method establishes a connection to the MongoDB instance specified by the URI provided
+     * as a system property and lists all available databases.
+     *
+     * @param args the command-line arguments (not used)
+     */
     public static void main(String[] args) {
+        // Retrieve the MongoDB URI from the system properties
         String connectionString = System.getProperty("mongodb.uri");
+
+        // Establish a connection to the MongoDB instance
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            // Retrieve the list of databases
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+
+            // Print information about each database
             databases.forEach(db -> System.out.println(db.toJson()));
         }
     }
