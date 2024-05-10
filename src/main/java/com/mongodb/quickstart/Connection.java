431dd3c5a966d1cee567eb3c5c20995a9d97e4b2
Andrea Moleri <andreamoleri@gmail.com>
2024-05-10 14:26:09 +0200

    Added Singleton Struture

--- src/main/java/com/mongodb/quickstart/Connection.java
+++ src/main/java/com/mongodb/quickstart/Connection.java
@@ -1,6 +1,3 @@
-/**
- * The Connection class provides functionality to connect to a MongoDB instance and list all available databases.
- */
 package com.mongodb.quickstart;
 
 import com.mongodb.client.MongoClient;
@@ -10,25 +7,58 @@ import org.bson.Document;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Connection class establishes a connection to a MongoDB instance using the provided connection string.
+ * It provides a singleton instance to ensure a single connection throughout the application.
+ */
 public class Connection {
+    private static Connection instance;
+    private final MongoClient mongoClient;
 
     /**
-     * The main method establishes a connection to the MongoDB instance specified by the URI provided
-     * as a system property and lists all available databases.
+     * Constructs a new Connection instance with the specified connection string.
      *
-     * @param args the command-line arguments (not used)
+     * @param connectionString the MongoDB connection string
+     */
+    private Connection(String connectionString) {
+        this.mongoClient = MongoClients.create(connectionString);
+    }
+
+    /**
+     * Returns the singleton instance of the Connection class, creating it if necessary.
+     *
+     * @param connectionString the MongoDB connection string
+     * @return the singleton instance of Connection
+     */
+    public static synchronized Connection getInstance(String connectionString) {
+        if (instance == null) {
+            instance = new Connection(connectionString);
+        }
+        return instance;
+    }
+
+    /**
+     * Retrieves and prints the list of databases available on the connected MongoDB instance.
+     */
+    public void listDatabases() {
+        // Retrieve the list of databases
+        List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+
+        // Print information about each database
+        databases.forEach(db -> System.out.println(db.toJson()));
+    }
+
+    /**
+     * Main method to establish a connection to MongoDB and list available databases.
+     *
+     * @param args command-line arguments (not used)
      */
     public static void main(String[] args) {
         // Retrieve the MongoDB URI from the system properties
         String connectionString = System.getProperty("mongodb.uri");
 
         // Establish a connection to the MongoDB instance
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            // Retrieve the list of databases
-            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-
-            // Print information about each database
-            databases.forEach(db -> System.out.println(db.toJson()));
-        }
+        Connection connection = Connection.getInstance(connectionString);
+        connection.listDatabases();
     }
 }
